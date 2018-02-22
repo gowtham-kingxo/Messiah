a735395fa8cb1b1566eaa5ca6ca5e1227bd689b9
Gowtham Genupur <gowtham.gourov@gmail.com>
2018-02-23 02:06:57 +0530

    just fb log in..in the process of firebase login

--- app/src/main/java/greenearth/united/com/messiah/MainActivity.java
+++ app/src/main/java/greenearth/united/com/messiah/MainActivity.java
@@ -1,14 +1,22 @@
 package greenearth.united.com.messiah;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public  class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     EditText username;
@@ -19,32 +27,79 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     String Entered_Username = null;
     String Entered_Password = null;
 
+
+
+    CallbackManager mCallbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        username  = (EditText) findViewById(R.id.username);
+        username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
 
         findViewById(R.id.submit).setOnClickListener(this);
-        findViewById(R.id.fbLogin).setOnClickListener(this);
 
-
-
-
+        // findViewById(R.id.fbLogin).setOnClickListener(this);
 
 
         mAuth = FirebaseAuth.getInstance();
+
+
+        // Initialize Facebook Login button
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("fb:", "facebook:onSuccess:" + loginResult);
+                // handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("fb:", "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+
+        });
+
+
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser != null)
+           updateUI();
+    }
+
+    private void updateUI() {
 
 
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     public void onClick(View view) {
-
-        
 
     }
 }
