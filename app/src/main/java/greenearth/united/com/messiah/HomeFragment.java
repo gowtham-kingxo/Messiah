595b1e73e316deebb89563c30b3f888b132ff734
Gowtham Genupur <gowtham.gourov@gmail.com>
2018-03-20 17:30:00 +0530

    Posts arranged date wise. Latest at the top.

--- app/src/main/java/greenearth/united/com/messiah/HomeFragment.java
+++ app/src/main/java/greenearth/united/com/messiah/HomeFragment.java
@@ -13,6 +13,7 @@ import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
@@ -57,9 +58,10 @@ public class HomeFragment extends Fragment {
 
         firebaseFirestore = FirebaseFirestore.getInstance();
 
+        Query firstQuery = firebaseFirestore.collection("Posts").orderBy("timestamp", Query.Direction.DESCENDING);
 
 
-            firebaseFirestore.collection("Posts").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            firstQuery.addSnapshotListener(new EventListener<QuerySnapshot>() {
 
                 @Override
                 public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
@@ -89,6 +91,7 @@ public class HomeFragment extends Fragment {
             return view;
 
     }
+
 
 
 }
