4a6fca447d1b00c8441a3263168b869ffdd94aa1
Gowtham Genupur <gowtham.gourov@gmail.com>
2018-03-20 23:05:25 +0530

    Part 14 over. Added pagination!! Works good!!

--- app/src/main/java/greenearth/united/com/messiah/HomeFragment.java
+++ app/src/main/java/greenearth/united/com/messiah/HomeFragment.java
@@ -7,9 +7,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
@@ -32,6 +34,8 @@ public class HomeFragment extends Fragment {
 
     private FirebaseAuth mAuth;
 
+    private DocumentSnapshot lastVisible;
+
 
 
     public HomeFragment() {
@@ -40,7 +44,7 @@ public class HomeFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
@@ -58,7 +62,25 @@ public class HomeFragment extends Fragment {
 
         firebaseFirestore = FirebaseFirestore.getInstance();
 
-        Query firstQuery = firebaseFirestore.collection("Posts").orderBy("timestamp", Query.Direction.DESCENDING);
+        volunteershp_list_view.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                Boolean reachedBottom = !recyclerView.canScrollVertically(1);
+
+                if(reachedBottom)
+                {
+                    String desc = lastVisible.getString("desc");
+                    Toast.makeText(container.getContext(), "Reched "+desc, Toast.LENGTH_SHORT).show();
+
+                    loadMorePost();
+                }
+            }
+        });
+
+        Query firstQuery = firebaseFirestore.collection("Posts")
+                .orderBy("timestamp", Query.Direction.DESCENDING).limit(3);
 
 
             firstQuery.addSnapshotListener(new EventListener<QuerySnapshot>() {
@@ -67,6 +89,8 @@ public class HomeFragment extends Fragment {
                 public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
                     if (mAuth.getCurrentUser() != null)
                     {
+                        lastVisible = documentSnapshots.getDocuments().get(documentSnapshots.size()-1);
+
                         for (DocumentChange doc : documentSnapshots.getDocumentChanges())
                         {
 
@@ -89,6 +113,47 @@ public class HomeFragment extends Fragment {
 
             // Inflate the layout for this fragment
             return view;
+
+    }
+
+    public void loadMorePost()
+    {
+        Query nextQuery = firebaseFirestore.collection("Posts")
+                .orderBy("timestamp", Query.Direction.DESCENDING)
+                .startAfter(lastVisible)
+                .limit(3);
+
+
+        nextQuery.addSnapshotListener(new EventListener<QuerySnapshot>() {
+
+            @Override
+            public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+                if (mAuth.getCurrentUser() != null)
+                {
+
+                    if(!documentSnapshots.isEmpty())
+                    {
+                        lastVisible = documentSnapshots.getDocuments().get(documentSnapshots.size() - 1);
+
+                        for (DocumentChange doc : documentSnapshots.getDocumentChanges())
+                        {
+
+                            if (mAuth.getCurrentUser() != null) {
+                                if (doc.getType() == DocumentChange.Type.ADDED)
+                                {
+                                    VolunteerPost volunteerPost = doc.getDocument().toObject(VolunteerPost.class);
+
+                                    volunteership_list.add(volunteerPost);
+
+                                    volunteerRecyclerAdaptor.notifyDataSetChanged();
+                                }
+                            }
+                        }
+                     }
+                }
+            }
+        });
+
 
     }
 
